1d69dec7fd7dda531795a55a3396e5ff9c524d89
ArkeologeN <hamza@plumtreegroup.net>
2012-08-30 18:51:31 +0500

    Initial Move

---
+++ app/code/local/MyCompany/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright (c) 2012 - 2013. All Rights Reserved.
+ * All the code underneath is written by Hamza Waqas @ Ubuntu
+ * From Karachi on The PlumTree Group.
+ * 
+ * *Code may varies from Public & Private source code.
+ */
+
+class MyCompany_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action {
+    
+    public function indexAction() {
+        echo 'Test Echo!';
+    }
+}
+?>
